721ec3cfee7ecdeaeac352b90683ac004f76ac91
裴宏悦 <hongyue.pei@yiducloud.cn>
2019-01-09 10:17:38 +0800

    lgbm

--- tcO2O/order2.0.py
+++ tcO2O/order2.0.py
@@ -130,7 +130,7 @@ def check_model(data, predictors):
         ('en', classifier())
     ])
 
-    print '-------------', data['label'].groupby(['label'], as_index=False).count()
+    print '-------------', data['label'].groupby('label', as_index=False).count()
 
     model.fit(data[predictors], data['label'])
 
